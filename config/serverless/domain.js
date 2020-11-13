74cf1e5da78b6440bcd1cd50f80c67330e5adf9e
cubone <cubone220@protonmail.com>
2020-11-12 21:42:50 -0600

    Update configuration

--- config/serverless/domain.js
+++ config/serverless/domain.js
@@ -1,8 +1,9 @@
 const _ = require("lodash");
 
 const DEFAULT = {
-  domainName: "api.yearn.tools",
-  certificateName: "yearn.tools",
+  domainName: "api.aries.financial",
+  certificateArn:
+    "arn:aws:acm:ap-southeast-1:973919765030:certificate/0bcc83ec-7965-4a26-b9ae-3c3dd99169cc",
   stage: "${self:custom.currentStage}",
   createRoute53Record: true,
   basePath: "${self:custom.basePath}",
